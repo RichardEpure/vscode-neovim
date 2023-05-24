4beb253cd2e97d1f6234592a876d416b8570412c
Richard <richardvandick@gmail.com>
2023-05-24 19:46:10 +0100

    tidy

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -5,25 +5,7 @@ vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
-
-    use {
-        'phaazon/hop.nvim',
-        branch = 'v2', -- optional but strongly recommended
-        config = function()
-          -- you can configure Hop the way you like here; see :h hop-config
-          require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
-        end
-    }
-
-    use({
-      'kylechui/nvim-surround',
-      tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-      config = function()
-          require("nvim-surround").setup({
-              -- Configuration here, or leave empty to use defaults
-          })
-      end
-  })
-
-  use "ggandor/leap.nvim"
+    use { 'phaazon/hop.nvim', branch = 'v2' }
+    use { 'kylechui/nvim-surround', tag = "*" }
+    use "ggandor/leap.nvim"
 end)
